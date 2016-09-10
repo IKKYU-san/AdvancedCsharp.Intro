e9a3f0e2e46a8c781736d936a8d1ab269b1bf034
Michele Andersson <michele@ikkyu.se>
2016-09-10 02:03:55 +0200

    Added OverloadedMethod1

---
+++ Introduction/OverloadedMethod1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedCsharp.Intro.Introduction
+{
+    class OverloadedMethod1
+    {
+        public static void Run()
+        {
+            Console.Write("Please enter a name: ");
+            string name = Console.ReadLine();
+
+            ShoutName(name);
+            ShoutName(name, 3);
+            ShoutName(name, 3, true);
+        }
+
+        public static void ShoutName(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void ShoutName(string s, int repeat)
+        {
+            string name = s.PadRight(s.Length + 1);
+            string output = String.Concat(Enumerable.Repeat(name, repeat));
+            Console.WriteLine(output);
+        }
+
+        public static void ShoutName(string s, int repeat, bool addStars = false)
+        {
+            if (addStars)
+            {
+                string name = s.PadRight(s.Length + 1);
+                string output = String.Concat(Enumerable.Repeat(name, repeat));
+                Console.WriteLine($"*********{output.TrimEnd()}*********");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AdvancedCsharp.Intro.Introduction
         {
             Console.ForegroundColor = ConsoleColor.White;
 
-            Dictionary2.Run();
+            OverloadedMethod1.Run();
 
             Console.ReadKey();
         }
